ad6d059da38bb609bc82d96699bde9361112da74
山根大樹 <32091@ojs.ac.jp>
2021-05-25 11:57:50 +0900

    p50リスト2.15 売上データー

--- SalesCalculator/Program.cs
+++ SalesCalculator/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,21 @@ namespace SalesCalculator {
     class Program {
         static void Main(string[] args) {
         }
+
+        static List<Sale> ReadSales(String filePath) {
+            List<Sale> sales = new List<Sale>();
+            string[] lines = File.ReadAllLines(filePath);
+            foreach(String line in lines){
+                string[] items = line.Split(',');
+                Sale sale = new Sale {
+                    ShopName = items[0],
+                    ProductCategory = items[1],
+                    Amount = int.Parse(items[2])
+                };
+                sales.Add(sale);
+            }
+            return sales;
+        }
+
     }
 }
