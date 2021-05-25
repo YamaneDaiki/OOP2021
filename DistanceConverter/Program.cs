9cd3d90ed88b36cec8c2e19df43ee3975fea6149
山根大樹 <32091@ojs.ac.jp>
2021-05-25 10:20:24 +0900

    p52 リスト26 変換処理をクラス化

--- DistanceConverter/Program.cs
+++ DistanceConverter/Program.cs
@@ -7,25 +7,36 @@ using System.Threading.Tasks;
 namespace DistanceConverter {
     class Program {
         static void Main(string[] args) {
-
             if (args.Length >= 1 && args[0] == "-tom") {
-                //フィールドからメートルへの対応表を出力         
-                for (int feet = 1; feet <= 10; feet++) {
-                    double meter = FeetToMeter(feet);
-                    Console.WriteLine("{0} ft = {1:0.0000} m", feet, meter);
-                }
+                // フィートからメートルへの対応表を出力
+                PrintFeetToMeterList(1, 10);
             } else {
-                for (int meter = 1; meter <= 10; meter++) {
-                    double feet = MeterToFeet(meter);
-                    Console.WriteLine("{0} m = {1:0.0000} ft", meter, feet);
-                }
+                // メートルからフィートへの対応表を出力
+                PrintMeterToFeetList(1, 10);
             }
         }
-       　static double FeetToMeter(int feet) {
+        // フィートからメートルへの対応表を出力
+        private static void PrintFeetToMeterList(int start, int stop) {
+            for (int feet = start; feet <= stop; feet++) {
+                double meter = FeetToMeter(feet);
+                Console.WriteLine("{0} ft = {1:0.0000} m", feet, meter);
+            }
+        }
+        // メートルからフィートへの対応表を出力
+        private static void PrintMeterToFeetList(int start, int stop) {
+            for (int meter = start; meter <= stop; meter++) {
+                double feet = MeterToFeet(meter);
+                Console.WriteLine("{0} m = {1:0.0000} ft", meter, feet);
+            }
+        }
+
+        // フィートからメートルを求める
+        static double FeetToMeter(int feet) {
             return feet * 0.3048;
         }
-         static double MeterToFeet(int meter) {
+        // メートルからフィートを求める
+        static double MeterToFeet(int meter) {
             return meter / 0.3048;
         }
-    }
+    }    
 }
