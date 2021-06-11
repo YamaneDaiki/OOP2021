2ab76de110c87362296f5d6ed73d6744b0c963b0
山根大樹 <32091@ojs.ac.jp>
2021-06-11 15:53:12 +0900

    p158 第5章

---
+++ Chapter05/Exercise１/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise１ {
+    class Program {
+        static void Main(string[] args) {
+            //5.1.1
+            var s1 = Console.ReadLine();
+            var s2 = Console.ReadLine();
+
+            if (String.Compare(s1, s2, true) == 0)
+                Console.WriteLine("等しいです");
+            else
+                Console.WriteLine("等しくないです");
+            //5.1.2
+            var s3 = Console.ReadLine();
+
+            int.TryParse(s3, out int height);
+            Console.WriteLine();
+
+            }
+        } 
+    }
