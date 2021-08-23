669690d67f20b7153d5993760d55f33676e58392
YamaneDaiki <84491816+YamaneDaiki@users.noreply.github.com>
2021-08-23 15:55:45 +0900

    Add files via upload

---
+++ Sample Online/Sample0823/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿
+namespace Sample0823
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Result = new System.Windows.Forms.TextBox();
+            this.Exec = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Jyou = new System.Windows.Forms.TextBox();
+            this.Value = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Result
+            // 
+            this.Result.Font = new System.Drawing.Font("MS UI Gothic", 19.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Result.Location = new System.Drawing.Point(1132, 161);
+            this.Result.Multiline = true;
+            this.Result.Name = "Result";
+            this.Result.Size = new System.Drawing.Size(191, 56);
+            this.Result.TabIndex = 11;
+            // 
+            // Exec
+            // 
+            this.Exec.Font = new System.Drawing.Font("MS UI Gothic", 16.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Exec.Location = new System.Drawing.Point(868, 155);
+            this.Exec.Name = "Exec";
+            this.Exec.Size = new System.Drawing.Size(184, 68);
+            this.Exec.TabIndex = 10;
+            this.Exec.Text = "結果→";
+            this.Exec.UseVisualStyleBackColor = true;
+            this.Exec.Click += new System.EventHandler(this.Exec_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 22.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(706, 155);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 59);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "乗";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 22.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(309, 158);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 59);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "の";
+            // 
+            // Jyou
+            // 
+            this.Jyou.Font = new System.Drawing.Font("MS UI Gothic", 19.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Jyou.Location = new System.Drawing.Point(420, 155);
+            this.Jyou.Multiline = true;
+            this.Jyou.Name = "Jyou";
+            this.Jyou.Size = new System.Drawing.Size(191, 56);
+            this.Jyou.TabIndex = 7;
+            // 
+            // Value
+            // 
+            this.Value.Font = new System.Drawing.Font("MS UI Gothic", 19.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Value.Location = new System.Drawing.Point(49, 155);
+            this.Value.Multiline = true;
+            this.Value.Name = "Value";
+            this.Value.Size = new System.Drawing.Size(191, 56);
+            this.Value.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1414, 870);
+            this.Controls.Add(this.Result);
+            this.Controls.Add(this.Exec);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Jyou);
+            this.Controls.Add(this.Value);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Result;
+        private System.Windows.Forms.Button Exec;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Jyou;
+        private System.Windows.Forms.TextBox Value;
+    }
+}
+
---
+++ Sample Online/Sample0823/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sample0823
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Exec_Click(object sender, EventArgs e) {
+            int n = int.Parse(Value.Text);
+            int p = int.Parse(Jyou.Text);
+            int result = n;
+            Math.Pow(n, p);
+            Result.Text = Math.Pow(n, p).ToString();
+        }
+    }
+}
