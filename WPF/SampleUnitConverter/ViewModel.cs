b4552f073154ad29a6818404f74bb2ab8b8f7f81
大樹 山根 <32091@ojs.ac.jp>
2021-11-12 15:56:12 +0900

    NumberGames　途中

---
+++ WPF/SampleUnitConverter/ViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace SampleUnitConverter {
+    public class ViewModel : INotifyPropertyChanged {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null) {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            //if (this.PropertyChanged != null)
+            //    this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
